c65b75d657c801cce9eb53b33febd6b6c89179b2
TruongIKPK <phitruong20192020@gmail.com>
2026-04-19 23:09:08 +0700

    init

---
+++ src/main/java/com/hamtech/bookstorenotificationservice/BookstoreNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstorenotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamtech/bookstorenotificationservice/BookstoreNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstorenotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
