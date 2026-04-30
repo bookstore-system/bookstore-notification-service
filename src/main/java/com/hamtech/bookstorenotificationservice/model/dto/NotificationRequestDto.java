5459aed0b67d197ab055f2d3e31b740dcb67a335
HinDayNi <gthithanhhien04@gmail.com>
2026-04-30 17:01:38 +0700

    feat: implement notification service infrastructure and order event DTOs

---
+++ src/main/java/com/hamtech/bookstorenotificationservice/model/dto/NotificationRequestDto.java
@@ -0,0 +1,48 @@
+package com.hamtech.bookstorenotificationservice.model.dto;
+
+public class NotificationRequestDto {
+    private String to;
+    private String subject;
+    private String message;
+
+    public NotificationRequestDto() {}
+
+    public NotificationRequestDto(String to, String subject, String message) {
+        this.to = to;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "NotificationRequestDto{" +
+                "to='" + to + '\'' +
+                ", subject='" + subject + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
