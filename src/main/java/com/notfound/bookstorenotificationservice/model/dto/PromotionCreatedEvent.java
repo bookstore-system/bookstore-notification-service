219747a5f2e8c895d6ed4da48b0d74b99a7eef98
HinDayNi <gthithanhhien04@gmail.com>
2026-05-21 09:35:33 +0700

    feat: create PromotionCreatedEvent class for handling promotion creation events

---
+++ src/main/java/com/notfound/bookstorenotificationservice/model/dto/PromotionCreatedEvent.java
@@ -0,0 +1,81 @@
+package com.notfound.bookstorenotificationservice.model.dto;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class PromotionCreatedEvent {
+
+    private UUID promotionId;
+    private String code;
+    private String name;
+    private String description;
+    private Double discountValue;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String status;
+
+    public UUID getPromotionId() {
+        return promotionId;
+    }
+
+    public void setPromotionId(UUID promotionId) {
+        this.promotionId = promotionId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getDiscountValue() {
+        return discountValue;
+    }
+
+    public void setDiscountValue(Double discountValue) {
+        this.discountValue = discountValue;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
